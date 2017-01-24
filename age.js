13cc76ff044b1f7219c4653aa7f51da3eba14ff2
Tory Shoreman <toryshoreman@gmail.com>
2017-01-24 23:21:15 +0000

    Adds looping exercises

---
+++ age.js
@@ -0,0 +1,13 @@
+
+
+function whichSchool(age) {
+  if (age < 13) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else {
+    return"Lighthouse Labs";
+  }
+}
+
+console.log(whichSchool(8))
---
+++ chorus-for.js
@@ -0,0 +1,7 @@
+var chorus = "Let's Dance!";
+
+for(var repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+
+console.log("Until the sun comes up!");
